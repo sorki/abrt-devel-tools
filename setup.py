fde2fa327c9ce1669ad3aea4df36a893ad37cb1d
Jiri Moskovcak <jmoskovc@redhat.com>
2013-09-16 16:56:15 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description=("Cmdline tool to ease the abrt devel process"),
     license = "GPLv2+",
     url = "https://github.com/mozeq/abrt-devel-tools",
-    scripts = ['src/abrt-devel-tools'],
+    scripts = ['src/submit-patch'],
     packages=['abrtdeveltools'],
     package_dir = {'abrtdeveltools': 'src/abrtdeveltools'},
 )
